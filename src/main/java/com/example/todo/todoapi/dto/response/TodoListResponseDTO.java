efdbe0df6b9186aa92ad5683bcb3a87c11c243b6
yajani <115179558+Yajani@users.noreply.github.com>
2023-06-02 12:04:50 +0900

    todo

--- src/main/java/com/example/todo/todoapi/dto/response/TodoListResponseDTO.java
+++ src/main/java/com/example/todo/todoapi/dto/response/TodoListResponseDTO.java
@@ -1,9 +1,18 @@
 package com.example.todo.todoapi.dto.response;
 
+import lombok.*;
+
 import java.util.List;
 
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TodoListResponseDTO {
-    private String error; //에러방생시 에러메시지를 담을 필드
-    private List<TodoDetailResponseDTO> todos;
 
+    private String error; // 에러발생시 에러메시지를 담을 필드
+    private List<TodoDetailResponseDTO> todos;
 }
